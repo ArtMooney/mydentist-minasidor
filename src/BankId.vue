6b82adf6684806511c8d1faeadd7528c11171138
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-10-03 16:30:30 +0200

    small cleanup for mobile test

--- src/BankId.vue
+++ src/BankId.vue
@@ -6,7 +6,6 @@
 <template>
   <div class="mydentist-app">
     <h3 style="background-color: coral">{{ message }}</h3>
-    <h3 style="background-color: coral">{{ message2 }}</h3>
     <div
       id="w-node-_5ef4a456-78ee-3356-a721-49f53fdd5c23-4be37fed"
       class="bankid-login-container"
@@ -77,23 +76,20 @@ export default {
       collectInterval: null,
       isLoginError: false,
       errorMessage: "Inloggningen misslyckades, var god försök igen!",
-      message: "Test v0.1.1",
-      message2: "",
+      message: "Test v0.1.2",
     };
   },
 
   created() {
     if (localStorage.getItem("U3>s^$9PX?V8Qzhv(yk_Zn") !== null) {
-      const localToken = JSON.parse(
-        localStorage.getItem("U3>s^$9PX?V8Qzhv(yk_Zn")
-      );
-
-      this.message = "REROUTED " + JSON.stringify(localToken);
       this.personNummer = JSON.parse(
         localStorage.getItem("U$YW+e3ahS;FM[c-Xx@tBR")
       );
 
-      this.startBankidCollect(localToken);
+      this.startBankidCollect(
+        JSON.parse(localStorage.getItem("U3>s^$9PX?V8Qzhv(yk_Zn"))
+      );
+
       localStorage.removeItem("U3>s^$9PX?V8Qzhv(yk_Zn");
       localStorage.removeItem("U$YW+e3ahS;FM[c-Xx@tBR");
     }
@@ -155,8 +151,6 @@ export default {
             "?orderRef=" +
             token.orderRef
         );
-
-        this.message2 = JSON.stringify(collect.status);
 
         if (collect.status === "complete") {
           this.stopBankidCollect();
